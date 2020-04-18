17e9a3c854be3c74f38798d500237d4ee7a2a7d3
keie <hombrehealth@hotmail.com>
2020-04-17 22:41:57 -0400

    jwt added

--- ApiCore/Startup.cs
+++ ApiCore/Startup.cs
@@ -65,11 +65,17 @@ namespace ApiCore
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            // CORS
+            // https://docs.asp.net/en/latest/security/cors.html
+            app.UseCors(builder =>
+                    builder.WithOrigins("http://localhost:4200", "http://www.myclientserver.com")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
             app.UseHttpsRedirection();
             app.UseAuthentication();
            
             app.UseMvc();
+            
         }
     }
 }
